58d2f4b4e5904d4d08cb8b438fb277211bb3408e
Olaniran Micheal <olaniranmicheal2020@gmail.com>
2023-05-14 16:03:37 +0100

    change title

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
+      <h1>UnityImage</h1>
       <h2>Your Pictures</h2>
       <p>Click the Button below to upload Image</p>
     </div>
